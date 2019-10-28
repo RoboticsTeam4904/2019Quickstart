afd8c5a071db08fbaf3851c6a1c8b3c72b75bbac
Daniel Bernstein <danbern@nuevaschool.org>
2019-10-28 13:28:44 -0700

    empty robotmap

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,11 +1,6 @@
 package org.usfirst.frc4904.robot;
 
-import org.usfirst.frc4904.standard.custom.controllers.CustomXbox;
-import org.usfirst.frc4904.standard.custom.motioncontrollers.CANTalonSRX;
 import org.usfirst.frc4904.standard.custom.sensors.PDP;
-import org.usfirst.frc4904.standard.subsystems.chassis.TankDrive;
-import org.usfirst.frc4904.standard.subsystems.motor.Motor;
-import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.AccelerationCap;
 
 /**
  * The RobotMap is a map of the physical robot. It stores all port numbers used
@@ -19,53 +14,66 @@ import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.Acceleration
 
 public class RobotMap {
 	public static class Port {
-		public static class Motors {
-			public static class CAN {
-				public static int leftDriveA = 1;
-				public static int leftDriveB = 2;
-				public static int rightDriveA = 3;
-				public static int rightDriveB = 4;
-			}
+		public static class HumanInput {
+
 		}
 
-		public static class HumanInput {
-			public static final int xboxController = 1;
+		public static class CANMotor {
+
+		}
+
+		public static class PWM {
+
+		}
+
+		public static class CAN {
+
+		}
+
+		public static class Pneumatics {
+
+		}
+
+		public static class Digital {
+
 		}
 	}
 
-	public static class Constant {
-		public static class HumanInput {
-			public static final double Y_SPEED_SCALE = 1;
-			public static final double TURN_SPEED_SCALE = 1;
-			public static final double XBOX_MINIMUM_THRESHOLD = 0.1;
-			public static final double SPEED_GAIN = 1;
-			public static final double SPEED_EXP = 2;
-			public static final double TURN_GAIN = 1;
-			public static final double TURN_EXP = 2;
+	public static class Metrics {
+		public static class Chassis {
+
+		}
+	}
+
+	public static class PID {
+		public static class Drive {
+
+		}
+
+		public static class Turn {
+
 		}
 	}
 
 	public static class Component {
-		public static Motor leftWheel;
-		public static Motor rightWheel;
-		public static TankDrive chassis;
 		public static PDP pdp;
+	}
+
+	public static class Input {
+
 	}
 
 	public static class HumanInput {
 		public static class Driver {
-			public static CustomXbox xbox;
+
+		}
+
+		public static class Operator {
+
 		}
 	}
 
 	public RobotMap() {
 		Component.pdp = new PDP();
-		Component.leftWheel = new Motor("LeftWheel", false, new AccelerationCap(Component.pdp),
-				new CANTalonSRX(Port.Motors.CAN.leftDriveA), new CANTalonSRX(Port.Motors.CAN.leftDriveB));
-		Component.rightWheel = new Motor("RightWheel", false, new AccelerationCap(Component.pdp),
-				new CANTalonSRX(Port.Motors.CAN.rightDriveA), new CANTalonSRX(Port.Motors.CAN.rightDriveB));
-		Component.chassis = new TankDrive("OffseasonChassis", Component.leftWheel, Component.rightWheel);
-		HumanInput.Driver.xbox = new CustomXbox(Port.HumanInput.xboxController);
-		HumanInput.Driver.xbox.setDeadZone(Constant.HumanInput.XBOX_MINIMUM_THRESHOLD);
 	}
 }
